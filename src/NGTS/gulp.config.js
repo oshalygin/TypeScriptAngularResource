42d4d20e247bd04f72828195c72ecf3e96cd440c
Oleg Shalygin <oshalygin@gmail.com>
2015-09-17 21:47:20 -0700

    added typescript related files such as tsd

---
+++ src/NGTS/gulp.config.js
@@ -0,0 +1,1 @@
+//TODO: Move all the ASP.NET 5 config related stuff to this file
---
+++ src/NGTS/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="toastr/toastr.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
